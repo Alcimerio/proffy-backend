e7e66df2ae5d84ccd4de1606a66def520071e53f
Alcimerio Rangel <alcimerioj@id.uff.br>
2020-08-08 03:56:49 -0300

    add more routes

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import routes from './routes.ts';
 
 const app = express();
 
-app.use(express.JSON());
+app.use(express.json());
+app.use(routes)
 
 app.listen(3333);
