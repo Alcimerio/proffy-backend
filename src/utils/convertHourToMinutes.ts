49f27ec5fba7b68d8be5ad70c514dfa529945886
Alcimerio Rangel <alcimerioj@id.uff.br>
2020-08-08 03:56:41 -0300

    add function to convert hour to minutes in /utils

---
+++ src/utils/convertHourToMinutes.ts
@@ -0,0 +1,5 @@
+export default function convertHourToMinutes(time: string) : Number {
+  const [hour, minutes] = time.split(':').map(Number)
+  const timeInMinutes = (hour * 60) + minutes;
+  return timeInMinutes;
+}
